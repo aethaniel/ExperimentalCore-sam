c3e007cab8de31197a934972c559152caebc6911
Thibaut VIARD <aethaniel@sam-geek.org>
2015-09-01 23:14:25 +0200

    [validation] blink app update

--- cores/validation/blink/blink.cpp
+++ cores/validation/blink/blink.cpp
@@ -2,18 +2,10 @@
 #include "Arduino.h"
 
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin LED_BUILTIN. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
+ * Blink
+ * Turns on an LED on for one second, then off for one second, repeatedly.
+ *
+ * Most boards have an on-board LED you can control which is attached to digital pin LED_BUILTIN.
  */
 
 
@@ -27,14 +19,13 @@ void setup(void)
 // the loop function runs over and over again forever
 void loop(void)
 {
-  int i;
+//  int i;
 
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+//  for (i=10000;i>0;i--) __NOP();
 
-  for (i=10000;i>0;i--) __NOP();
-
-//  delay(1000);              // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-//  delay(1000);              // wait for a second
-  for (i=10000;i>0;i--) __NOP();
+  delay(1000);              // wait for a second
+//  for (i=10000;i>0;i--) __NOP();
 }
