aa4824f89fd37964b416fd87e3cc8d98cf93682e
Thibaut VIARD <aethaniel@sam-geek.org>
2015-09-03 18:29:21 +0200

    [arch/arm] Introducing Cortex-M only vectors

---
+++ cores/arduino/arch_arm/core_cortex_vectors.c
@@ -0,0 +1,165 @@
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "sam.h"
+#include "core_hooks.h"
+#include "core_cortex_vectors.h"
+#include "core_variant.h"
+#include "core_delay.h"           // for SysTick_DefaultHandler()
+#include <stdint.h>
+
+/* Symbols exported from linker script */
+extern uint32_t __etext ;
+extern uint32_t __data_start__ ;
+extern uint32_t __data_end__ ;
+extern uint32_t __bss_start__ ;
+extern uint32_t __bss_end__ ;
+extern uint32_t __StackTop;
+
+extern int main( void ) ;
+/* symbols from libc */
+extern void __libc_init_array(void);
+
+
+/**
+ * \brief Default interrupt handler for unused IRQs.
+ */
+static void _halt(void)
+{
+  // Halts
+  while (1);
+}
+
+/**
+ * \brief This is the code that gets called on processor reset.
+ * Initializes the device and call the main() routine.
+ */
+void Reset_Handler( void )
+{
+  uint32_t *pSrc, *pDest;
+
+  /* Initialize the initialized data section */
+  pSrc = &__etext;
+  pDest = &__data_start__;
+
+  if ( (&__data_start__ != &__data_end__) && (pSrc != pDest) )
+  {
+    for (; pDest < &__data_end__ ; pDest++, pSrc++ )
+    {
+      *pDest = *pSrc ;
+    }
+  }
+
+  /* Clear the zero section */
+  if ( &__bss_start__ != &__bss_end__ )
+  {
+    for ( pDest = &__bss_start__ ; pDest < &__bss_end__ ; pDest++ )
+    {
+      *pDest = 0ul ;
+    }
+  }
+
+  /* Initialize the C library */
+//  __libc_init_array();
+
+  /* Initialize the system */
+  SystemInit() ;
+
+  /* Branch to main function */
+  main() ;
+
+  /* Infinite loop */
+  while ( 1 )
+  {
+  }
+}
+
+#if defined DEBUG && (DEBUG == 1)
+#warning "DEBUG handlers activated"
+
+void HardFault_Handler (void)
+{
+  while (1);
+}
+
+void NMI_Handler(void)
+{
+  while (1);
+}
+
+void MemManage_Handler(void)
+{
+  while (1);
+}
+
+void BusFault_Handler(void)
+{
+  while (1);
+}
+
+void UsageFault_Handler(void)
+{
+  while (1);
+}
+
+void DebugMon_Handler(void)
+{
+  while (1);
+}
+
+#else
+
+void HardFault_Handler (void) __attribute__ ((weak, alias("_halt")));
+void NMI_Handler       (void) __attribute__ ((weak, alias("_halt")));
+void MemManage_Handler (void) __attribute__ ((weak, alias("_halt")));
+void BusFault_Handler  (void) __attribute__ ((weak, alias("_halt")));
+void UsageFault_Handler(void) __attribute__ ((weak, alias("_halt")));
+void DebugMon_Handler  (void) __attribute__ ((weak, alias("_halt")));
+
+#endif // DEBUG=1
+
+void SVC_Handler(void)
+{
+  svcHook();
+}
+
+void PendSV_Handler(void)
+{
+  pendSVHook();
+}
+
+void SysTick_Handler(void)
+{
+  if (sysTickHook())
+    return;
+  SysTick_DefaultHandler();
+}
+
+/* Exception Table */
+__attribute__ ((section(".isr_vector")))
+const CoreVectors startup_exception_table=
+{
+  /* Configure Initial Stack Pointer, using linker-generated symbols */
+  .pvStack = (void*) (&__StackTop),
+
+  .pfnReset_Handler      = (void*) Reset_Handler,
+  .pfnNMI_Handler        = (void*) NMI_Handler,
+  .pfnHardFault_Handler  = (void*) HardFault_Handler,
+  .pfnMemManage_Handler  = (void*) MemManage_Handler,
+  .pfnBusFault_Handler   = (void*) BusFault_Handler,
+  .pfnUsageFault_Handler = (void*) UsageFault_Handler,
+  .pfnReserved1_Handler  = (void*) (0UL),          /* Reserved */
+  .pfnReserved2_Handler  = (void*) (0UL),          /* Reserved */
+  .pfnReserved3_Handler  = (void*) (0UL),          /* Reserved */
+  .pfnReserved4_Handler  = (void*) (0UL),          /* Reserved */
+  .pfnSVC_Handler        = (void*) SVC_Handler,
+  .pfnDebugMon_Handler   = (void*) DebugMon_Handler,
+  .pfnReserved5_Handler  = (void*) (0UL),          /* Reserved */
+  .pfnPendSV_Handler     = (void*) PendSV_Handler,
+  .pfnSysTick_Handler    = (void*) SysTick_Handler,
+ };
+
+#ifdef __cplusplus
+}
+#endif
