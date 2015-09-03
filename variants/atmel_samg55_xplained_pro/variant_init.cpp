e3a3a42854ffb272e7ad6f6faa627bd6462ea9b0
Thibaut VIARD <aethaniel@sam-geek.org>
2015-09-03 20:01:46 +0200

    [variant] Updating SAMG55 Xplained Pro

--- variants/atmel_samg55_xplained_pro/variant_init.cpp
+++ variants/atmel_samg55_xplained_pro/variant_init.cpp
@@ -17,7 +17,15 @@
 */
 
 #include "Arduino.h"
+#include "core_private.h"
 
 void initVariant( void )
 {
+  // Initialize Serial port Flexcom6 pins
+  pinPeripheral(PIN_SERIAL_RX, GPIO_PERIPH_B);
+  digitalWrite(PIN_SERIAL_RX, HIGH); // Enable pullup for RXD6
+  pinPeripheral(PIN_SERIAL_TX, GPIO_PERIPH_B);
+
+  // TODO: move this to Serial class!
+  FLEXCOM6->FLEXCOM_MR=FLEXCOM_MR_OPMODE_USART;
 }
