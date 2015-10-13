7ba8f29436651010dd6538ef1343e30939457e80
Thibaut VIARD <aethaniel@sam-geek.org>
2015-10-13 19:13:30 +0200

    [all] move variant definitions into dedicated makefile

---
+++ variants/atmel_sam4s_xplained/variant.mk
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------------------------
+#		Variant information
+#-------------------------------------------------------------------------------
+
+VARIANT_NAME = atmel_sam4s_xplained
+DEVICE_PART = __SAM4S16C__
+DEVICE_CORE = cortex-m4
+DEVICE_FREQUENCY = 120000000ul
