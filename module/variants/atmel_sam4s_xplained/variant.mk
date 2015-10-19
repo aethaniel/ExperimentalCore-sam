b4047ec4dd9ffa79b4b480a9b479e62581f740a1
Thibaut VIARD <aethaniel@sam-geek.org>
2015-10-19 21:26:23 +0200

    [build] bringing dependencies for more accurate build

--- module/variants/atmel_sam4s_xplained/variant.mk
+++ module/variants/atmel_sam4s_xplained/variant.mk
@@ -6,3 +6,4 @@ VARIANT_NAME = atmel_sam4s_xplained
 DEVICE_PART = __SAM4S16C__
 DEVICE_CORE = cortex-m4
 DEVICE_FREQUENCY = 120000000ul
+
--- module/variants/atmel_samg55_xplained_pro/variant.mk
+++ module/variants/atmel_samg55_xplained_pro/variant.mk
@@ -6,3 +6,4 @@ VARIANT_NAME = atmel_samg55_xplained_pro
 DEVICE_PART = __SAMG55J19__
 DEVICE_CORE = cortex-m4
 DEVICE_FREQUENCY = 120000000ul
+
