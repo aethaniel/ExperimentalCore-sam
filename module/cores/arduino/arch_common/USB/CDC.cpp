562045d4bfa34b14e6aa8826f17bf38f40598095
Thibaut VIARD <aethaniel@sam-geek.org>
2015-10-31 20:53:55 +0100

    [core/USB] Adding as temporary branch the USB device port from https://github.com/flutterwireless/ArduinoCodebase

---
+++ module/cores/arduino/arch_common/USB/CDC.cpp
@@ -0,0 +1,453 @@
+/* Copyright (c) 2011, Peter Barrett
+**
+** Permission to use, copy, modify, and/or distribute this software for
+** any purpose with or without fee is hereby granted, provided that the
+** above copyright notice and this permission notice appear in all copies.
+**
+** THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+** WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+** BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+** OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+** WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+** ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+** SOFTWARE.
+*/
+
+#include "Arduino.h"
+#include "USBAPI.h"
+#include "Reset.h"
+
+#ifdef CDC_ENABLED
+
+#ifndef __SAM3S1A__
+#define CDC_SERIAL_BUFFER_SIZE	512
+#else
+//Moved to USBAPI.h so user sketch can see it.
+//#define CDC_SERIAL_BUFFER_SIZE	1024 
+#endif//SAM3S1A
+
+/* For information purpose only since RTS is not always handled by the terminal application */
+#define CDC_LINESTATE_DTR		0x01 // Data Terminal Ready
+#define CDC_LINESTATE_RTS		0x02 // Ready to Send
+
+#define CDC_LINESTATE_READY		(CDC_LINESTATE_RTS | CDC_LINESTATE_DTR)
+
+struct ring_buffer
+{
+	uint8_t buffer[CDC_SERIAL_BUFFER_SIZE];
+	volatile uint32_t head;
+	volatile uint32_t tail;
+};
+
+ring_buffer cdc_rx_buffer = { { 0 }, 0, 0};
+
+typedef struct
+{
+	uint32_t	dwDTERate;
+	uint8_t		bCharFormat;
+	uint8_t 	bParityType;
+	uint8_t 	bDataBits;
+	uint8_t		lineState;
+} LineInfo;
+
+static volatile LineInfo _usbLineInfo = { 
+    57600, // dWDTERate
+    0x00,  // bCharFormat
+    0x00,  // bParityType
+    0x08,  // bDataBits
+    0x00   // lineState
+};
+
+_Pragma("pack(1)")
+static const CDCDescriptor _cdcInterface =
+{
+	D_IAD(0,2,CDC_COMMUNICATION_INTERFACE_CLASS,CDC_ABSTRACT_CONTROL_MODEL,1),
+
+	//	CDC communication interface
+	D_INTERFACE(CDC_ACM_INTERFACE,1,CDC_COMMUNICATION_INTERFACE_CLASS,CDC_ABSTRACT_CONTROL_MODEL,0),
+	D_CDCCS(CDC_HEADER,0x10,0x01),								// Header (1.10 bcd)
+	D_CDCCS(CDC_CALL_MANAGEMENT,1,1),							// Device handles call management (not)
+	D_CDCCS4(CDC_ABSTRACT_CONTROL_MANAGEMENT,6),				// SET_LINE_CODING, GET_LINE_CODING, SET_CONTROL_LINE_STATE supported
+	D_CDCCS(CDC_UNION,CDC_ACM_INTERFACE,CDC_DATA_INTERFACE),	// Communication interface is master, data interface is slave 0
+	D_ENDPOINT(USB_ENDPOINT_IN (CDC_ENDPOINT_ACM),USB_ENDPOINT_TYPE_INTERRUPT,0x10, 0x10),
+
+	//	CDC data interface
+	D_INTERFACE(CDC_DATA_INTERFACE,2,CDC_DATA_INTERFACE_CLASS,0,0),
+#ifndef __SAM3S1A__
+	D_ENDPOINT(USB_ENDPOINT_OUT(CDC_ENDPOINT_OUT),USB_ENDPOINT_TYPE_BULK,512,0),
+	D_ENDPOINT(USB_ENDPOINT_IN (CDC_ENDPOINT_IN ),USB_ENDPOINT_TYPE_BULK,512,0)
+#else
+//#else
+	D_ENDPOINT(USB_ENDPOINT_OUT(CDC_ENDPOINT_OUT),USB_ENDPOINT_TYPE_BULK,64,0),
+	D_ENDPOINT(USB_ENDPOINT_IN (CDC_ENDPOINT_IN ),USB_ENDPOINT_TYPE_BULK,64,0)
+#endif
+};
+static const CDCDescriptor _cdcOtherInterface =
+{
+	D_IAD(0,2,CDC_COMMUNICATION_INTERFACE_CLASS,CDC_ABSTRACT_CONTROL_MODEL,1),
+
+	//	CDC communication interface
+	D_INTERFACE(CDC_ACM_INTERFACE,1,CDC_COMMUNICATION_INTERFACE_CLASS,CDC_ABSTRACT_CONTROL_MODEL,0),
+	D_CDCCS(CDC_HEADER,0x10,0x01),								// Header (1.10 bcd)
+	D_CDCCS(CDC_CALL_MANAGEMENT,1,1),							// Device handles call management (not)
+	D_CDCCS4(CDC_ABSTRACT_CONTROL_MANAGEMENT,6),				// SET_LINE_CODING, GET_LINE_CODING, SET_CONTROL_LINE_STATE supported
+	D_CDCCS(CDC_UNION,CDC_ACM_INTERFACE,CDC_DATA_INTERFACE),	// Communication interface is master, data interface is slave 0
+	D_ENDPOINT(USB_ENDPOINT_IN (CDC_ENDPOINT_ACM),USB_ENDPOINT_TYPE_INTERRUPT,0x10, 0x10),
+
+	//	CDC data interface
+	D_INTERFACE(CDC_DATA_INTERFACE,2,CDC_DATA_INTERFACE_CLASS,0,0),
+	D_ENDPOINT(USB_ENDPOINT_OUT(CDC_ENDPOINT_OUT),USB_ENDPOINT_TYPE_BULK,64,0),
+	D_ENDPOINT(USB_ENDPOINT_IN (CDC_ENDPOINT_IN ),USB_ENDPOINT_TYPE_BULK,64,0)
+};
+_Pragma("pack()")
+
+int WEAK CDC_GetInterface(uint8_t* interfaceNum)
+{
+	interfaceNum[0] += 2;	// uses 2
+	return USBD_SendControl(0,&_cdcInterface,sizeof(_cdcInterface));
+}
+
+int WEAK CDC_GetOtherInterface(uint8_t* interfaceNum)
+{
+	interfaceNum[0] += 2;	// uses 2
+	return USBD_SendControl(0,&_cdcOtherInterface,sizeof(_cdcOtherInterface));
+}
+
+bool WEAK CDC_Setup(Setup& setup)
+{
+	uint8_t r = setup.bRequest;
+	uint8_t requestType = setup.bmRequestType;
+
+	if (REQUEST_DEVICETOHOST_CLASS_INTERFACE == requestType)
+	{
+		if (CDC_GET_LINE_CODING == r)
+		{
+			USBD_SendControl(0,(void*)&_usbLineInfo,7);
+			return true;
+		}
+	}
+
+	if (REQUEST_HOSTTODEVICE_CLASS_INTERFACE == requestType)
+	{
+		if (CDC_SET_LINE_CODING == r)
+		{
+			USBD_RecvControl((void*)&_usbLineInfo,7);
+			return true;
+		}
+
+		if (CDC_SET_CONTROL_LINE_STATE == r)
+		{
+			_usbLineInfo.lineState = setup.wValueL;
+			// auto-reset into the bootloader is triggered when the port, already
+			// open at 1200 bps, is closed.
+			if (1200 == _usbLineInfo.dwDTERate)
+			{
+				// We check DTR state to determine if host port is open (bit 0 of lineState).
+				if ((_usbLineInfo.lineState & 0x01) == 0)
+					initiateReset(250);
+				else
+					cancelReset();
+			}
+			return true;
+		}
+	}
+	return false;
+}
+
+int _serialPeek = -1;
+void Serial_::begin(uint32_t baud_count)
+{
+	// suppress "unused parameter" warning
+	(void)baud_count;
+}
+
+void Serial_::begin(uint32_t baud_count, uint8_t config)
+{
+	// suppress "unused parameter" warning
+	(void)baud_count;
+	(void)config;
+}
+
+void Serial_::end(void)
+{
+}
+
+void Serial_::accept(void)
+{
+	static uint32_t guard = 0;
+
+	// synchronized access to guard
+	do {
+		if (__LDREXW(&guard) != 0) {
+			__CLREX();
+			return;  // busy
+		}
+	} while (__STREXW(1, &guard) != 0); // retry until write succeed
+	ring_buffer *buffer = &cdc_rx_buffer;
+	#ifdef __SAM3S1A__
+
+	//The number of bytes available in the fifo. When data is popped from the fifo, the hardware
+	//leaves RXBYTECNT unaltered (it does not decrement). Therefore, all bytes must be read from
+	//the fifo at once, and then the fifo must be cleared. So we have to check the ring buffer
+	//to see if there is enough room to accommodate the bytes available in the fifo. If not, then
+	//we can't read the fifo, and we have to block further writes from the host. In this situation,
+	//the RX interrupt will keep retriggering until there is room in the ring buffer to accept the data.
+	//Because of this, we have to disable CDC_RX interrupt to allow user code to read the data (see Serial_:read()).
+	//Short version: Must read entire fifo on the SAM3S or nothing at all. 
+	uint32_t len;
+	uint32_t bytes_free = CDC_SERIAL_BUFFER_SIZE - this->available(); // bytes free in ring buffer
+	uint32_t fifo_count = USBD_Available(CDC_RX); //bytes available to be read in SAM3S fifo
+	if((fifo_count) && (bytes_free > fifo_count)) {
+		//bytes available in fifo, and enough space in ring buffer. Read the available bytes into the ring buffer.
+		uint8_t data_tmp[64];//can't read more than 64 bytes on CDC_RX
+		len = USBD_Recv(CDC_RX,&data_tmp,fifo_count); //read all available bytes and clear fifo
+		//Now copy data into ring buffer
+		for(int i=0; i<len; i++)
+		{
+			buffer->buffer[buffer->head] = data_tmp[i];
+			buffer->head = (uint32_t)(buffer->head+1) % CDC_SERIAL_BUFFER_SIZE;
+		}
+	}
+
+	#else
+	uint32_t i = (uint32_t)(buffer->head+1) % CDC_SERIAL_BUFFER_SIZE;
+	// if we should be storing the received character into the location
+	// just before the tail (meaning that the head would advance to the
+	// current location of the tail), we're about to overflow the buffer
+	// and so we don't write the character or advance the head.
+	while (i != buffer->tail) {
+		uint32_t c;
+		if (!USBD_Available(CDC_RX)) {
+			udd_ack_fifocon(CDC_RX);
+			break;
+		}
+		c = USBD_Recv(CDC_RX);
+		// c = UDD_Recv8(CDC_RX & 0xF);
+		buffer->buffer[buffer->head] = c;
+		buffer->head = i;
+
+		i = (i + 1) % CDC_SERIAL_BUFFER_SIZE;
+	}
+	#endif
+
+	// release the guard
+	guard = 0;
+}
+
+int Serial_::available(void)
+{
+	ring_buffer *buffer = &cdc_rx_buffer;
+	return (unsigned int)(CDC_SERIAL_BUFFER_SIZE + buffer->head - buffer->tail) % CDC_SERIAL_BUFFER_SIZE;
+}
+
+int Serial_::peek(void)
+{
+	ring_buffer *buffer = &cdc_rx_buffer;
+
+	if (buffer->head == buffer->tail)
+	{
+		return -1;
+	}
+	else
+	{
+		return buffer->buffer[buffer->tail];
+	}
+}
+
+#ifdef __SAM3S1A__
+//extended Serial class in USBAPI.h to provide this bulk read method.
+//User code must supply a pointer to receive the data. The number of
+//bytes actually read is returned. The idea is to quere SerialUSB.available()
+//and pass it in as len to this method to read all available bytes from the 
+//ring buffer in one shot. This should realize full SAM3S transmit bandwidth.
+
+//Here is an example sketch to demonstrate:
+/*
+	char rx_bytes[CDC_SERIAL_BUFFER_SIZE];
+	int len;
+	String bytes_read;
+
+	void setup() {
+	  SerialUSB.begin(9600); 
+	}
+
+	void loop() {
+	  if (SerialUSB.available() > 0) {
+	    bytes_read = "";
+	    len = SerialUSB.available();
+	    len = SerialUSB.read((uint8_t*)&rx_bytes,len);
+	    for (int i=0;i<len;i++){
+	      bytes_read = bytes_read + rx_bytes[i];
+	    }
+	    SerialUSB.print("you typed ");
+	    SerialUSB.println(bytes_read);
+	  }
+	}
+*/
+int Serial_::read(uint8_t* d, uint32_t len)
+{
+	ring_buffer* buffer = &cdc_rx_buffer;
+	
+	// if the head isn't ahead of the tail, we don't have any characters
+	if (buffer->head == buffer->tail)
+	{
+		return 0;
+	}
+	else
+	{
+		uint8_t* ptr_dest = d;
+		uint32_t bytes_avail = this->available(); //number of bytes available in ring buffer
+		uint32_t fifo_count = USBD_Available(CDC_RX); //bytes available to be read in SAM3S fifo
+		//Don't try to read more bytes than are available in the ring buffer
+		if (len > bytes_avail) {
+			len = bytes_avail;
+		}
+		
+		//Now read the bytes from the ring buffer into the user's requested location
+		uint32_t i;
+		for (i = 0; i < len; ++i) {
+			*ptr_dest++ = buffer->buffer[buffer->tail];
+			buffer->tail = (uint32_t)(buffer->tail+1) % CDC_SERIAL_BUFFER_SIZE;
+		}
+		
+		//Now see if there is more data available from the host
+		if (fifo_count) {
+			//read fifo into ring buffer
+			accept();
+		} else {
+			//All bytes read from fifo and buffer. Turn calling of accept() back over to interrupt.
+			if (!this->available()){
+				udd_enable_endpoint_interrupt(CDC_RX);
+			}
+		}
+		
+	}
+	return len;
+}
+#endif //SAM3S1A
+
+
+
+int Serial_::read(void)
+{
+	ring_buffer *buffer = &cdc_rx_buffer;
+
+	// if the head isn't ahead of the tail, we don't have any characters
+	if (buffer->head == buffer->tail)
+	{
+		return -1;
+	}
+	else
+	{
+		unsigned char c = buffer->buffer[buffer->tail];
+		buffer->tail = (unsigned int)(buffer->tail + 1) % CDC_SERIAL_BUFFER_SIZE;
+		#ifdef __SAM3S1A__
+		//The first call of accept() is triggered by the CDC_RX interrupt. The interrupt is desabled 
+		//and the data is read into the ring buffer. This allows user code to call Serial_::read() to 
+		//drain the ring buffer. Otherwise, the interrupts would continue to pre-empt user code, 
+		//preventing user code from reading the data, and finally the buffer would fill and deadlock would result.
+		//Each read() checks again for data available in the fifo. If there is data there and the ring buffer has
+		//room, accept() will be called again to draw the fifo data into the ring buffer. Finally when there
+		//is no more data in the fifo (nothing coming in from the host) the interrupt is re-enabled to wake up if 
+		//the host sends more data later, and the process is started all over again. So the main problem here is 
+		//that the host can fill the fifo in 64 byte chunks, but Serial_:read() can only read one byte at a time. 
+		//This means the host is blocked from sending unless the ring buffer capacity is increased.
+		//There is no implementation here that can read the entire buffer. Is there? This cuts read bandwidth significantly.
+		//Seems like user code should be able to call Serial_::available() to determine the byte count, and pull it out in one
+		//call. ex: Serial_::read(&data,count). Such a method could return the actual number of bytes read just in case.
+		uint32_t bytes_free = CDC_SERIAL_BUFFER_SIZE - this->available(); //bytes free in ring buffer
+		uint32_t fifo_count = USBD_Available(CDC_RX); //bytes available to be read in SAM3S fifo
+		if (fifo_count) {
+			if (bytes_free > fifo_count) {
+				//read fifo into ring buffer
+				accept();
+			}
+		} else {
+			//All bytes read from fifo and buffer. Turn calling of accept() back over to interrupt.
+			if (!this->available()){
+				udd_enable_endpoint_interrupt(CDC_RX);
+			}
+		}
+		#else	
+		if (USBD_Available(CDC_RX))
+			accept();
+		#endif
+		
+		return c;
+	}
+}
+
+void Serial_::flush(void)
+{
+	USBD_Flush(CDC_TX);
+}
+
+size_t Serial_::write(const uint8_t *buffer, size_t size)
+{
+	/* only try to send bytes if the high-level CDC connection itself
+	 is open (not just the pipe) - the OS should set lineState when the port
+	 is opened and clear lineState when the port is closed.
+	 bytes sent before the user opens the connection or after
+	 the connection is closed are lost - just like with a UART. */
+
+	// TODO - ZE - check behavior on different OSes and test what happens if an
+	// open connection isn't broken cleanly (cable is yanked out, host dies
+	// or locks up, or host virtual serial port hangs)
+	if (_usbLineInfo.lineState > 0)
+	{
+		int r = USBD_Send(CDC_TX, buffer, size);
+
+		if (r > 0)
+		{
+			return r;
+		} else
+		{
+			setWriteError();
+			return 0;
+		}
+	}
+	setWriteError();
+	return 0;
+}
+
+size_t Serial_::write(uint8_t c) {
+	return write(&c, 1);
+}
+
+// This operator is a convenient way for a sketch to check whether the
+// port has actually been configured and opened by the host (as opposed
+// to just being connected to the host).  It can be used, for example, in
+// setup() before printing to ensure that an application on the host is
+// actually ready to receive and display the data.
+// We add a short delay before returning to fix a bug observed by Federico
+// where the port is configured (lineState != 0) but not quite opened.
+Serial_::operator bool()
+{
+	// this is here to avoid spurious opening after upload
+	if (millis() < 500)
+		return false;
+
+	bool result = false;
+
+	if (_usbLineInfo.lineState > 0)
+	{
+		result = true;
+	}
+
+	delay(10);
+	return result;
+}
+
+Serial_ SerialUSB;
+
+#ifdef __SAM3S1A__
+//Allow USBCore.cpp to fetch the configuration descriptor
+const CDCDescriptor* CDC_GetDescriptor(void){
+	return &_cdcInterface;
+}
+#endif
+
+#endif //CDC_ENABLED
+
+
---
+++ module/cores/arduino/arch_common/USB/USBCore.h
@@ -0,0 +1,363 @@
+// Copyright (c) 2010, Peter Barrett
+/*
+** Permission to use, copy, modify, and/or distribute this software for
+** any purpose with or without fee is hereby granted, provided that the
+** above copyright notice and this permission notice appear in all copies.
+**
+** THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+** WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR
+** BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES
+** OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+** WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+** ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+** SOFTWARE.
+*/
+
+#ifndef __USBCORE_H__
+#define __USBCORE_H__
+
+//	Standard requests
+#define GET_STATUS					0
+#define CLEAR_FEATURE				1
+#define SET_FEATURE					3
+#define SET_ADDRESS					5
+#define GET_DESCRIPTOR				6
+#define SET_DESCRIPTOR				7
+#define GET_CONFIGURATION			8
+#define SET_CONFIGURATION			9
+#define GET_INTERFACE				10
+#define SET_INTERFACE				11
+
+
+// bmRequestType
+#define REQUEST_HOSTTODEVICE		0x00
+#define REQUEST_DEVICETOHOST		0x80
+#define REQUEST_DIRECTION			0x80
+
+#define REQUEST_STANDARD			0x00
+#define REQUEST_CLASS				0x20
+#define REQUEST_VENDOR				0x40
+#define REQUEST_TYPE				0x60
+
+#define REQUEST_DEVICE				0x00
+#define REQUEST_INTERFACE			0x01
+#define REQUEST_ENDPOINT			0x02
+#define REQUEST_OTHER				0x03
+#define REQUEST_RECIPIENT			0x1F
+
+#define REQUEST_DEVICETOHOST_CLASS_INTERFACE  (REQUEST_DEVICETOHOST + REQUEST_CLASS + REQUEST_INTERFACE)
+#define REQUEST_HOSTTODEVICE_CLASS_INTERFACE  (REQUEST_HOSTTODEVICE + REQUEST_CLASS + REQUEST_INTERFACE)
+
+//	Class requests
+
+#define CDC_SET_LINE_CODING			0x20
+#define CDC_GET_LINE_CODING			0x21
+#define CDC_SET_CONTROL_LINE_STATE	0x22
+
+#define MSC_RESET					0xFF
+#define MSC_GET_MAX_LUN				0xFE
+
+#define HID_GET_REPORT				0x01
+#define HID_GET_IDLE				0x02
+#define HID_GET_PROTOCOL			0x03
+#define HID_SET_REPORT				0x09
+#define HID_SET_IDLE				0x0A
+#define HID_SET_PROTOCOL			0x0B
+
+//	Descriptors
+
+#define USB_DEVICE_DESC_SIZE 18
+#define USB_CONFIGUARTION_DESC_SIZE 9
+#define USB_INTERFACE_DESC_SIZE 9
+#define USB_ENDPOINT_DESC_SIZE 7
+
+#define USB_DEVICE_DESCRIPTOR_TYPE             1
+#define USB_CONFIGURATION_DESCRIPTOR_TYPE      2
+#define USB_STRING_DESCRIPTOR_TYPE             3
+#define USB_INTERFACE_DESCRIPTOR_TYPE          4
+#define USB_ENDPOINT_DESCRIPTOR_TYPE           5
+#define USB_DEVICE_QUALIFIER                   6
+#define USB_OTHER_SPEED_CONFIGURATION          7
+
+#define USB_DEVICE_CLASS_COMMUNICATIONS        0x02
+#define USB_DEVICE_CLASS_HUMAN_INTERFACE       0x03
+#define USB_DEVICE_CLASS_STORAGE               0x08
+#define USB_DEVICE_CLASS_VENDOR_SPECIFIC       0xFF
+
+#define USB_CONFIG_POWERED_MASK                0x40
+#define USB_CONFIG_BUS_POWERED                 0x80
+#define USB_CONFIG_SELF_POWERED                0xC0
+#define USB_CONFIG_REMOTE_WAKEUP               0x20
+
+// bMaxPower in Configuration Descriptor
+#define USB_CONFIG_POWER_MA(mA)                ((mA)/2)
+
+// bEndpointAddress in Endpoint Descriptor
+#define USB_ENDPOINT_DIRECTION_MASK            0x80
+#define USB_ENDPOINT_OUT(addr)                 ((addr) | 0x00)
+#define USB_ENDPOINT_IN(addr)                  ((addr) | 0x80)
+
+#define USB_ENDPOINT_TYPE_MASK                 0x03
+#define USB_ENDPOINT_TYPE_CONTROL              0x00
+#define USB_ENDPOINT_TYPE_ISOCHRONOUS          0x01
+#define USB_ENDPOINT_TYPE_BULK                 0x02
+#define USB_ENDPOINT_TYPE_INTERRUPT            0x03
+
+#define TOBYTES(x) ((x) & 0xFF),(((x) >> 8) & 0xFF)
+
+#define CDC_V1_10                               0x0110
+#define CDC_COMMUNICATION_INTERFACE_CLASS       0x02
+
+
+#define CDC_CALL_MANAGEMENT                     0x01
+#define CDC_ABSTRACT_CONTROL_MODEL              0x02
+#define CDC_HEADER                              0x00
+#define CDC_ABSTRACT_CONTROL_MANAGEMENT         0x02
+#define CDC_UNION                               0x06
+#define CDC_CS_INTERFACE                        0x24
+#define CDC_CS_ENDPOINT                         0x25
+#define CDC_DATA_INTERFACE_CLASS                0x0A
+
+#define MSC_SUBCLASS_SCSI						0x06
+#define MSC_PROTOCOL_BULK_ONLY					0x50
+
+#define HID_HID_DESCRIPTOR_TYPE					0x21
+#define HID_REPORT_DESCRIPTOR_TYPE				0x22
+#define HID_PHYSICAL_DESCRIPTOR_TYPE			0x23
+
+_Pragma("pack(1)")
+
+//	Device
+typedef struct {
+	uint8_t len;				// 18
+	uint8_t dtype;				// 1 USB_DEVICE_DESCRIPTOR_TYPE
+	uint16_t usbVersion;		// 0x200
+	uint8_t	deviceClass;
+	uint8_t	deviceSubClass;
+	uint8_t	deviceProtocol;
+	uint8_t	packetSize0;		// Packet 0
+	uint16_t	idVendor;
+	uint16_t	idProduct;
+	uint16_t	deviceVersion;	// 0x100
+	uint8_t	iManufacturer;
+	uint8_t	iProduct;
+	uint8_t	iSerialNumber;
+	uint8_t	bNumConfigurations;
+} DeviceDescriptor;
+
+//	Config
+typedef struct {
+	uint8_t	len;			// 9
+	uint8_t	dtype;			// 2
+	uint16_t clen;			// total length
+	uint8_t	numInterfaces;
+	uint8_t	config;
+	uint8_t	iconfig;
+	uint8_t	attributes;
+	uint8_t	maxPower;
+} ConfigDescriptor;
+
+//	String
+
+//	Interface
+typedef struct
+{
+	uint8_t len;		// 9
+	uint8_t dtype;		// 4
+	uint8_t number;
+	uint8_t alternate;
+	uint8_t numEndpoints;
+	uint8_t interfaceClass;
+	uint8_t interfaceSubClass;
+	uint8_t protocol;
+	uint8_t iInterface;
+} InterfaceDescriptor;
+
+//	Endpoint
+typedef struct
+{
+	uint8_t len;		// 7
+	uint8_t dtype;		// 5
+	uint8_t addr;
+	uint8_t attr;
+	uint16_t packetSize;
+	uint8_t interval;
+} EndpointDescriptor;
+
+// Interface Association Descriptor
+// Used to bind 2 interfaces together in CDC compostite device
+typedef struct
+{
+	uint8_t len;				// 8
+	uint8_t dtype;				// 11
+	uint8_t firstInterface;
+	uint8_t interfaceCount;
+	uint8_t functionClass;
+	uint8_t funtionSubClass;
+	uint8_t functionProtocol;
+	uint8_t iInterface;
+} IADDescriptor;
+
+//	CDC CS interface descriptor
+typedef struct
+{
+	uint8_t len;		// 5
+	uint8_t dtype;		// 0x24
+	uint8_t subtype;
+	uint8_t d0;
+	uint8_t d1;
+} CDCCSInterfaceDescriptor;
+
+typedef struct
+{
+	uint8_t len;		// 4
+	uint8_t dtype;		// 0x24
+	uint8_t subtype;
+	uint8_t d0;
+} CDCCSInterfaceDescriptor4;
+
+typedef struct
+{
+    uint8_t	len;
+    uint8_t 	dtype;		// 0x24
+    uint8_t 	subtype;	// 1
+    uint8_t 	bmCapabilities;
+    uint8_t 	bDataInterface;
+} CMFunctionalDescriptor;
+
+typedef struct
+{
+    uint8_t	len;
+    uint8_t 	dtype;		// 0x24
+    uint8_t 	subtype;	// 1
+    uint8_t 	bmCapabilities;
+} ACMFunctionalDescriptor;
+
+typedef struct
+{
+	//	IAD
+	IADDescriptor				iad;	// Only needed on compound device
+
+	//	Control
+	InterfaceDescriptor			cif;
+	CDCCSInterfaceDescriptor	header;
+	CMFunctionalDescriptor		callManagement;			// Call Management
+	ACMFunctionalDescriptor		controlManagement;		// ACM
+	CDCCSInterfaceDescriptor	functionalDescriptor;	// CDC_UNION
+	EndpointDescriptor			cifin;
+
+	//	Data
+	InterfaceDescriptor			dif;
+	EndpointDescriptor			in;
+	EndpointDescriptor			out;
+} CDCDescriptor;
+
+typedef struct
+{
+	InterfaceDescriptor			msc;
+	EndpointDescriptor			in;
+	EndpointDescriptor			out;
+} MSCDescriptor;
+
+typedef struct
+{
+	uint8_t len;			// 9
+	uint8_t dtype;			// 0x21
+	uint8_t addr;
+	uint8_t	versionL;		// 0x101
+	uint8_t	versionH;		// 0x101
+	uint8_t	country;
+	uint8_t	desctype;		// 0x22 report
+	uint8_t	descLenL;
+	uint8_t	descLenH;
+} HIDDescDescriptor;
+
+typedef struct
+{
+	InterfaceDescriptor		hid;
+	HIDDescDescriptor		desc;
+	EndpointDescriptor		in;
+} HIDDescriptor;
+
+_Pragma("pack()")
+
+#define D_DEVICE(_class,_subClass,_proto,_packetSize0,_vid,_pid,_version,_im,_ip,_is,_configs) \
+	{ 18, 1, 0x200, _class,_subClass,_proto,_packetSize0,_vid,_pid,_version,_im,_ip,_is,_configs }
+
+#define D_CONFIG(_totalLength,_interfaces) \
+	{ 9, 2, _totalLength,_interfaces, 1, 0, USB_CONFIG_SELF_POWERED, USB_CONFIG_POWER_MA(500) }
+
+#define D_OTHERCONFIG(_totalLength,_interfaces) \
+	{ 9, 7, _totalLength,_interfaces, 1, 0, USB_CONFIG_SELF_POWERED, USB_CONFIG_POWER_MA(500) }
+
+#define D_INTERFACE(_n,_numEndpoints,_class,_subClass,_protocol) \
+	{ 9, 4, _n, 0, _numEndpoints, _class,_subClass, _protocol, 0 }
+
+#define D_ENDPOINT(_addr,_attr,_packetSize, _interval) \
+	{ 7, 5, _addr,_attr,_packetSize, _interval }
+
+#define D_QUALIFIER(_class,_subClass,_proto,_packetSize0,_configs) \
+	{ 10, 6, 0x200, _class,_subClass,_proto,_packetSize0,_configs }
+
+#define D_IAD(_firstInterface, _count, _class, _subClass, _protocol) \
+	{ 8, 11, _firstInterface, _count, _class, _subClass, _protocol, 0 }
+
+#define D_HIDREPORT(_descriptorLength) \
+	{ 9, 0x21, 0x1, 0x1, 0, 1, 0x22, _descriptorLength, 0 }
+
+#define D_CDCCS(_subtype,_d0,_d1)	{ 5, 0x24, _subtype, _d0, _d1 }
+#define D_CDCCS4(_subtype,_d0)		{ 4, 0x24, _subtype, _d0 }
+
+#endif
+
+#ifdef __SAM3S1A__
+/** Atmel helper functions from compiler.h used in udp_device.h*/
+typedef uint32_t                U32;  //!< 32-bit unsigned integer.
+#if (defined __GNUC__) || (defined __CC_ARM)
+#   define ctz(u)              __builtin_ctz(u)
+#else
+#   define ctz(u)              ((u) & (1ul <<  0) ?  0 : \
+                                (u) & (1ul <<  1) ?  1 : \
+                                (u) & (1ul <<  2) ?  2 : \
+                                (u) & (1ul <<  3) ?  3 : \
+                                (u) & (1ul <<  4) ?  4 : \
+                                (u) & (1ul <<  5) ?  5 : \
+                                (u) & (1ul <<  6) ?  6 : \
+                                (u) & (1ul <<  7) ?  7 : \
+                                (u) & (1ul <<  8) ?  8 : \
+                                (u) & (1ul <<  9) ?  9 : \
+                                (u) & (1ul << 10) ? 10 : \
+                                (u) & (1ul << 11) ? 11 : \
+                                (u) & (1ul << 12) ? 12 : \
+                                (u) & (1ul << 13) ? 13 : \
+                                (u) & (1ul << 14) ? 14 : \
+                                (u) & (1ul << 15) ? 15 : \
+                                (u) & (1ul << 16) ? 16 : \
+                                (u) & (1ul << 17) ? 17 : \
+                                (u) & (1ul << 18) ? 18 : \
+                                (u) & (1ul << 19) ? 19 : \
+                                (u) & (1ul << 20) ? 20 : \
+                                (u) & (1ul << 21) ? 21 : \
+                                (u) & (1ul << 22) ? 22 : \
+                                (u) & (1ul << 23) ? 23 : \
+                                (u) & (1ul << 24) ? 24 : \
+                                (u) & (1ul << 25) ? 25 : \
+                                (u) & (1ul << 26) ? 26 : \
+                                (u) & (1ul << 27) ? 27 : \
+                                (u) & (1ul << 28) ? 28 : \
+                                (u) & (1ul << 29) ? 29 : \
+                                (u) & (1ul << 30) ? 30 : \
+                                (u) & (1ul << 31) ? 31 : \
+                                32)
+#endif
+#define Rd_bits( value, mask)        ((value) & (mask))
+#define Rd_bitfield( value, mask)           (Rd_bits( value, mask) >> ctz(mask))
+#define Tst_bits( value, mask)  (Rd_bits(value, mask) != 0)
+#define Wr_bitfield(lvalue, mask, bitfield) (Wr_bits(lvalue, mask, (U32)(bitfield) << ctz(mask)))
+#define Wr_bits(lvalue, mask, bits)  ((lvalue) = ((lvalue) & ~(mask)) |\
+                                                 ((bits  ) &  (mask)))
+#define Set_bits(lvalue, mask)  ((lvalue) |=  (mask))
+#define Clr_bits(lvalue, mask)  ((lvalue) &= ~(mask))
+#endif //SAM3S1A
+
